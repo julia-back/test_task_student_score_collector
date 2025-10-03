98acbdcbb3f400eccbf575d08051db97eba57e5b
Julia Bengert <yubengert@gmail.com>
2025-10-03 17:58:58 +0300

    add response_model on routers

--- app/scores/routers.py
+++ app/scores/routers.py
@@ -1,24 +1,25 @@
 from fastapi import APIRouter
+from scores.schemas import Score
 
 
 router = APIRouter(prefix="/scores", tags=["Scores"])
 
 
-@router.get("/")
+@router.get("/", response_model=list[Score])
 def get_list_scores():
     pass
 
 
-@router.post("/new/")
+@router.post("/new/", response_model=Score)
 def create_score():
     pass
 
 
-@router.get("/{score_id}/")
-def get_score():
+@router.get("/{score_id}/", response_model=Score)
+def get_score(score_id: int):
     pass
 
 
-@router.patch("/{score_id}/update/")
-def update_score():
+@router.patch("/{score_id}/update/", response_model=Score)
+def update_score(score_id: int):
     pass
--- app/users/routers.py
+++ app/users/routers.py
@@ -1,10 +1,11 @@
 from fastapi import APIRouter
 from users.schemas import CreateUser
+from users.schemas import User
 
 
 router = APIRouter(prefix="/users", tags=["Users"])
 
 
-@router.post("/register/")
+@router.post("/register/", response_model=User)
 def register_user(user: CreateUser):
     pass
