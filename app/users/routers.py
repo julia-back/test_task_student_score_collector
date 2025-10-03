b072b6439c5e232fd18c29c374efc93f01f8689d
Julia Bengert <yubengert@gmail.com>
2025-10-03 16:14:38 +0300

    add user schema

--- app/users/routers.py
+++ app/users/routers.py
@@ -1,9 +1,10 @@
 from fastapi import APIRouter
+from users.schemas import CreateUser
 
 
 router = APIRouter(prefix="/users", tags=["Users"])
 
 
 @router.post("/register/")
-def register_user():
+def register_user(user: CreateUser):
     pass
