24b9fab000fe06086f1fc1306037ad0ddafb0658
Julia Bengert <yubengert@gmail.com>
2025-10-04 13:50:56 +0300

    add lifespan app for db engine dispose

--- app/main.py
+++ app/main.py
@@ -3,9 +3,18 @@ from fastapi import FastAPI
 from scores.routers import router as scores_router
 from users.routers import router as users_router
 from config import settings
+from contextlib import asynccontextmanager
+from database import db_manager
 
 
-app = FastAPI()
+@asynccontextmanager
+async def lifespan(fastapi_app: FastAPI):
+    yield
+    print("dispose engine")
+    await db_manager.dispose_engine()
+
+
+app = FastAPI(lifespan=lifespan)
 app.include_router(scores_router)
 app.include_router(users_router)
 
