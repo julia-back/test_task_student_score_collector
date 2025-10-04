8bda33466efeafe89d675b40394f9730c587989e
Julia Bengert <yubengert@gmail.com>
2025-10-04 16:23:23 +0300

    add model User

---
+++ app/users/models.py
@@ -0,0 +1,12 @@
+from database import Base
+from sqlalchemy.orm import Mapped, mapped_column
+from sqlalchemy import String, BigInteger
+
+
+class User(Base):
+    __tablename__ = "users"
+
+    id: Mapped[int] = mapped_column(primary_key=True, unique=True, index=True)
+    first_name: Mapped[str]
+    last_name: Mapped[str]
+    telegram_id: Mapped[str] = mapped_column(BigInteger, unique=True, index=True)
